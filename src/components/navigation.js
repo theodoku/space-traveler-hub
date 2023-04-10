f5cfe2389e895d4eb23eb398f4323fc623e10b41
ilyass atlassi <atlassiilyass@gmail.com>
2023-04-10 16:45:38 +0000

    Add dependencie

---
+++ src/components/navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import './styles/navigation.css'
+
+function Nav() {
+  return (
+    <nav>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
