a0bd041d6038e615ba087581707d64a58871af37
ilyass atlassi <atlassiilyass@gmail.com>
2023-04-10 16:51:38 +0000

    Add dependencie

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "../assets/logo.png";
-import './styles/navigation.css'
+import React from 'react';
+import logo from '../assets/logo.png';
+import './styles/navigation.css';
 
 function Nav() {
   return (
