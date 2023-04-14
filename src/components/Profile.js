4c3f36eb044749f7bfaac33cc3d85fd1b679a3cb
theophilus doku <theodoku89@gmail.com>
2023-04-14 10:15:14 +0000

    Modified profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import ProfileMission from './ProfileMisison';
+import RocketsProfile from '../Pages/RocketsProfile';
 
 export default function Profile() {
   const { mission, isLoading } = useSelector((state) => state.mission);
@@ -8,17 +9,20 @@ export default function Profile() {
       <div className="content">
         <h2>My Missions</h2>
         <p id="mission-not-taken" />
-        {mission && mission.find((item) => 'reserved' in item && item.reserved === true) ? (
-          mission.map((item) => (item.reserved && (
-            <div key={item.mission_id} className="item">
-              <ProfileMission mission={item} />
-            </div>
-          )
-          ))
-        ) : (
-          <div>No Missions Taken</div>
-        )}
+        {mission
+        && mission.find((item) => 'reserved' in item && item.reserved === true) ? (
+            mission.map(
+              (item) => item.reserved && (
+                <div key={item.mission_id} className="item">
+                  <ProfileMission mission={item} />
+                </div>
+              ),
+            )
+          ) : (
+            <div>No Missions Taken</div>
+          )}
         {isLoading && <div>Loading...</div>}
+        <RocketsProfile />
       </div>
     </div>
   );
